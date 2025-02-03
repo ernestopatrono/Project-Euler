163cdf010212c12bee4f190482adcf910ccc2eff
ernes <ernestopatrono@gmail.com>
2025-02-03 15:55:38 +0100

    finished problem 2

--- problema2.py
+++ problema2.py
@@ -3,14 +3,14 @@
 Problem #2
 """
 
-limit = 4000000
+LIMIT = 4000000
 
-n = 1,1
-
+m, n = 1, 1
 sum = 0
-while n[1] < limit:
-    if n[1]%2 == 0:
-        sum += n[1]
-    n = n[1], n[0] + n[1]
-    
+
+while n < LIMIT:
+    if n % 2 == 0:
+        sum += n
+    m, n = n, m + n
+
 print(sum)
