1a40997e640d0c8f031f607c77ef0601ae1af636
ernes <ernestopatrono@gmail.com>
2025-02-03 01:32:34 +0100

    finished problem 1, now really, last time was mistake

--- problema1.py
+++ problema1.py
@@ -8,11 +8,11 @@ limit = 1000
 factors = 3 , 5
 
 sum = 0
+
 for n in range(limit):
     for factor in factors:
-        print(n, factor)
         if n % factor == 0:
             sum += n
-            print("sum "+str(sum))
             break
+        
 print(sum)
