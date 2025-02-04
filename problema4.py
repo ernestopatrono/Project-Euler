f9a32f97d0da01e22ee1e4e01d0550a842c42ea1
ernes <ernestopatrono@gmail.com>
2025-02-04 17:08:36 +0100

    completed problem 4

--- problema4.py
+++ problema4.py
@@ -3,6 +3,19 @@
 Problem #4
 """
 
-x=47689675
+DIGITS = 7
 
+largest_palindrome = 100 ** (DIGITS-1)
 
+lower_bound = 10 ** (DIGITS-1)
+
+for x in range(10 ** DIGITS - 1, lower_bound - 1, -1):
+    for y in range(10 ** DIGITS - 1, x - 1, -1):
+        prod = x * y
+        if prod <= largest_palindrome:
+            break
+        if str(prod) == str(prod)[::-1]:
+            largest_palindrome = prod
+            lower_bound = x
+                
+print(largest_palindrome)
